a83af99d5393ea05a23846cfc0b055eeb2150175
Syahid Maisya <syahidmaisya@gmail.com>
2026-06-10 17:19:28 +0700

    debug: surface runtime error

--- api/index.php
+++ api/index.php
@@ -1,15 +1,21 @@
 <?php
 
-// Laravel butuh folder writable; di Vercel hanya /tmp yang bisa ditulis
+// Paksa tampilkan error biar ketahuan penyebab 500
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+// Laravel butuh folder writable; di Vercel cuma /tmp yang bisa ditulis
 $dirs = [
     '/tmp/storage/framework/views',
     '/tmp/storage/framework/cache',
     '/tmp/storage/framework/sessions',
     '/tmp/storage/logs',
+    '/tmp/bootstrap/cache',
 ];
 foreach ($dirs as $dir) {
     if (!is_dir($dir)) {
-        mkdir($dir, 0755, true);
+        @mkdir($dir, 0777, true);
     }
 }
 
